ee5ec90e24ef91d5f418f3c3777344cc500ac73e
redkostia <redkostia@mail.ru>
2016-07-07 18:40:57 +0300

    small fixes

--- BitlyTest.Data/Models/Link.cs
+++ BitlyTest.Data/Models/Link.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace BitlyTest.Data.Models
 	public class Link
 	{
 		public int Id { get; set; }
+		[MaxLength(2083)]
 		public string OriginalUrl { get; set; }
+		[MaxLength(32)]
 		public string ShortUrl { get; set; }
 		public DateTime Created { get; set; }
 
--- BitlyTest/Controllers/HomeController.cs
+++ BitlyTest/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using System.Web.Routing;
 using BitlyTest.Bll.Services;
 
 namespace BitlyTest.Controllers
@@ -11,21 +12,17 @@ namespace BitlyTest.Controllers
 		{
 			_bitlyService = bitlyService;
 		}
-		//public ActionResult Index()
-		//{
-		//	ViewBag.Title = "Home Page";
-			
-		//	//ViewBag.Links = links;
-			
-		//}
 
 		public ActionResult Index(string url = null)
 		{
-			//_bitlyService.Add("http://ya.ru");
+			if (string.IsNullOrEmpty(url))
+			{
+				return View();
+			}
 			var originalUrl = _bitlyService.LogVisit(url);
 			if (string.IsNullOrEmpty(originalUrl))
 			{
-				return View();
+				return Redirect("/");
 			}
 			return Redirect(originalUrl);
 		}
